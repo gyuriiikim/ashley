7aed9df06a7c310d12c272127227f04b9b0cebb6
gyuri <bitgyu@gmail.com>
2023-05-13 17:14:22 +0900

    0513

--- scripte/main.js
+++ scripte/main.js
@@ -1,4 +1,5 @@
-const swiper = new Swiper(".bnr", {
+// main bnr swiper
+const swiper1 = new Swiper(".bnr", {
     spaceBetween: 30,
     centeredSlides: true,
     loop:true,
@@ -10,4 +11,23 @@ const swiper = new Swiper(".bnr", {
         el: ".swiper-pagination",
         clickable: true,
     }
+});
+
+// event bnr swiper
+const swiper2 = new Swiper(".event_slide", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
